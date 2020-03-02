c185697f8a04fa75222eef047eb9c9597c596b22
Inoyatullo <you@example.com>
2020-03-02 17:23:47 +0500

    replaced style.css in style.min.css

--- gulpfile.js
+++ gulpfile.js
@@ -1,5 +1,7 @@
 const gulp = require ('gulp');
 const browserSync = require('browser-sync').create();
+const cssmin = require('gulp-cssmin');
+const rename = require('gulp-rename');
 
 gulp.task('hello', function(done) {
   console.log('привет, мир!');
@@ -14,4 +16,12 @@ gulp.task('browser-sync', function() {
       }
   });
   gulp.watch("./*.html").on('change', browserSync.reload);
+});
+
+gulp.task('replace', function (done) {
+  gulp.src('./css/**/*.css')
+      .pipe(cssmin())
+      .pipe(rename({suffix: '.min'}))
+      .pipe(gulp.dest('./css'));
+      done(); 
 });
